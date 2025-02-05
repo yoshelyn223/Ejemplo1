bb8acf21e5c407bfcc7e39772206b388eec3574b
Android_Esparza <esparzadamaris88@gmail.com>
2025-02-05 13:50:21 -0600

    Programa Base 05-02-2025

---
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.ejemplo1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.ejemplo1.ui.theme.Ejemplo1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ejemplo1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "TEC",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Feliz Cumpleaños $name!", modifier = modifier,
+                fontSize = 30.sp, lineHeight = 116.sp)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ejemplo1Theme {
+        Greeting("TEC")
+    }
+}
