8de6a8bb881e6335a123c7c8e010e010ea210b4f
Android_Esparza <esparzadamaris88@gmail.com>
2025-02-11 13:51:45 -0600

    TextField, Label y placeHolder C-11-02-2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
@@ -20,27 +29,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "TEC",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
+
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Feliz Cumpleaños $name!", modifier = modifier,
-                fontSize = 30.sp, lineHeight = 116.sp)
+fun GreetingPreview(){
+    Column {
+        TextFieldSencillo()
+        TextFieldPlaceHolder()
+        TextFieldKeyboard()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("TEC")
+fun TextFieldSencillo(){
+    var text by remember { mutableStateOf("") }
+    Column {
+    TextField(
+        value = text,
+        onValueChange = {newText ->text = newText},
+        label = { Text("Introduce tu nombre") }
+    )
+        Text("Tu nombre es: $text")
     }
+}
+
+
+@Composable
+fun TextFieldPlaceHolder(){
+    var text by remember { mutableStateOf("") }
+    Column {
+        TextField(
+            value = text,
+            onValueChange = {newText ->text = newText},
+
+            label = { Text("Nombre") },
+            placeholder = { Text("Introduce tu nombre") }
+        )
+        Text("Tu nombre es: $text")
+    }
+}
+
+@Composable
+fun TextFieldKeyboard(){
+    var text by remember { mutableStateOf("") }
+Column {
+    TextField(
+        value = text,
+        onValueChange = {newText -> text = newText},
+        label = { Text("Numero") },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+    Text("Tu nombre es: $text")
+}
 }
